3c68e1dd28dab78401b53a9d1c4c9812fa562484
AdamTorvikStaffen <astaffen@gmail.com>
2021-06-10 01:20:50 -0700

    added buttons to 2nd page

---
+++ nba-insider/src/main/resources/static/myscripts.js
@@ -0,0 +1,10 @@
+function myFunction() {
+  //alert("Fucka you!");
+  document.getElementById("page-1").classList.add("hidden")
+  document.getElementById("page-2").classList.remove("hidden")
+  
+}
+function teamButton(){
+  alert('hellothere!')
+  document.getElementById("myDropdown").classList.toggle("show");
+}
