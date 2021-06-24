ce99bb0426745b876bac7ac3684c9ca4605934a0
Adam Staffen <astaffen@gmail.com>
2021-06-24 01:13:15 -0700

    Jason added, request function, fixed CSS

--- nba-insider/src/main/resources/static/myscripts.js
+++ nba-insider/src/main/resources/static/myscripts.js
@@ -1,5 +1,5 @@
-function myFunction() {
-  //alert("Fucka you!");
+function secondPage() {
+  jason('GET', '/nba')
   document.getElementById("page-1").classList.add("hidden")
   document.getElementById("page-2").classList.remove("hidden")
   
@@ -7,8 +7,24 @@ function myFunction() {
 function teamButton(){
   alert('hellothere!')
   document.getElementById("myDropdown").classList.toggle("show");
-<<<<<<< Updated upstream
 }
-=======
+
+function reqListener () {
+  const response = JSON.parse(this.responseText, null, 2);
+  return response
+
 }
->>>>>>> Stashed changes
+
+function jason(method, url){
+  let get = new XMLHttpRequest();
+  get.addEventListener("load", reqListener);
+  get.open(method, url);
+  get.send();
+}
+
+
+
+function playerTog(){
+  obj = reqListener
+  
+}
