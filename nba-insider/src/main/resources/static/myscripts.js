d5a117fc0f0723f745e1b1360c7850a0a6635004
Adam Staffen <astaffen@gmail.com>
2021-07-08 01:37:06 -0700

    working on CSS and adding player toggle photos.

--- nba-insider/src/main/resources/static/myscripts.js
+++ nba-insider/src/main/resources/static/myscripts.js
@@ -1,30 +1,52 @@
+let globalJsonObject = {}
+
 function secondPage() {
   jason('GET', '/nba')
   document.getElementById("page-1").classList.add("hidden")
   document.getElementById("page-2").classList.remove("hidden")
   
 }
-function teamButton(){
-  alert('hellothere!')
-  document.getElementById("myDropdown").classList.toggle("show");
-}
 
-function reqListener () {
-  const response = JSON.parse(this.responseText, null, 2);
-  return response
+
+function reqListener() { //parsing jason and saving in global verable
+  globalJsonObject = JSON.parse(this.responseText);
 
 }
 
-function jason(method, url){
+function jason(method, url) {
   let get = new XMLHttpRequest();
   get.addEventListener("load", reqListener);
   get.open(method, url);
   get.send();
 }
 
-
-
-function playerTog(){
-  obj = reqListener
-  
+function choosePlayerButton(id) {
+  let toggle = ''
+  teamNames = Object.keys(globalJsonObject).sort()
+  teamNames.forEach(team => {
+    let img = `<img class="team-logo" id="${team}" onClick="teamPlayers(this.id)"  src="https://d2p3bygnnzw9w3.cloudfront.net/req/202106153/tlogo/bbr/${team}-2021.png" alt=${team} team Logo" />`
+    toggle += `<li title="${team}">${img}</li>`
+  });
+  document.getElementById(`${id}-dropdown`).innerHTML = toggle
+  document.getElementById(`${id}-dropdown`).classList.toggle("show");
+   
 }
+
+function teamPlayers(id) {
+  let playerList = ''
+  
+  team = Object.keys(globalJsonObject[`${id}`])
+  team.forEach(player => {
+    console.log(player)
+    let photo = globalJsonObject[`${id}`][`${player}`].Pic
+    if (player != "Logo"){
+      let img = `<img class="player-photo' src="https://www.basketball-reference.com/req/202106291/images/players/${photo}.jpg" `
+      console.log(img)
+    }
+
+  });
+}
+//ideas for menu: desktop keep team logos on side as you select players
+
+//on phone when you select a team players pop up and teams icons disapear. click team button starts over, back to team button locked at top of toggle. 
+//flex box a few rows 
