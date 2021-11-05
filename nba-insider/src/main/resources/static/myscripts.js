c3f594266f3306b71aa66951d9db1790a62add90
Adam Staffen <astaffen@gmail.com>
2021-11-04 17:38:53 -0700

    working with the boys 11/4 and adding QOL stuff

--- nba-insider/src/main/resources/static/myscripts.js
+++ nba-insider/src/main/resources/static/myscripts.js
@@ -35,6 +35,7 @@ function jason(method, url) {
 function chooseTeamButton(buttonID) {
   
   teamState.state=buttonID
+  document.getElementById(teamState.state).src = 'png/bball.svg';
   let toggle = ''
   teamNames = Object.keys(globalJsonObject).sort()
   teamNames.forEach(team => {
@@ -44,50 +45,62 @@ function chooseTeamButton(buttonID) {
   document.getElementById(`${buttonID}-dropdown`).innerHTML = toggle
   document.getElementById(`${buttonID}-dropdown`).classList.toggle("show");
   
-   
 }
 
 function teamPlayers(id) {
-  teamState.state == "teamOne" ? teamState.teamOne.franchise = id : teamState.teamTwo.franchise =id;
-  document.getElementById(teamState.state).src = `https://d2p3bygnnzw9w3.cloudfront.net/req/202106153/tlogo/bbr/${id}-2021.png`
-  let playerList = ''
-  let team = Object.keys(globalJsonObject[`${id}`])
-  let team1 = document.getElementById("teamOne-dropdown"); //used to remove from the dom
-  let team2 = document.getElementById("teamTwo-dropdown"); // need to find a cleaner way...
+  teamState.state == "teamOne" ? teamState.teamOne.franchise = id : teamState.teamTwo.franchise = id;
+  document.getElementById(teamState.state).src = `https://d2p3bygnnzw9w3.cloudfront.net/req/202106153/tlogo/bbr/${id}-2021.png`;
+  resetPicks(teamState.state)
+
+  let playerList = '';
+  const team = Object.keys(globalJsonObject[`${id}`]);
+  const team1 = document.getElementById("teamOne-dropdown"); //used to remove from the dom
+  const team2 = document.getElementById("teamTwo-dropdown"); // need to find a cleaner way...
   team.forEach(player => {
     if (player != "Logo"){
-      let playerID = globalJsonObject[`${id}`][`${player}`]
-      let photo = playerID.Pic
-      let name = playerID.Name
-      
-      let img = `<img src="https://www.basketball-reference.com/req/202106291/images/players/${photo}.jpg" class="player-photo">`
-      playerList += `<div onclick="selectedPlayers(this.id)" class="player-cards" id="${name}", title="${name}, ${id}">${img}<div class="player-info"><p>Name: ${name}</p><p>Salary: ${playerID.Salary}</p></div></div>`
-    }
+      const playerID = globalJsonObject[`${id}`][`${player}`];
+      const photo = playerID.Pic;
+      const name = playerID.Name;
+      const img = `<img src="https://www.basketball-reference.com/req/202106291/images/players/${photo}.jpg" class="player-photo">`;
+      playerList += `<div onclick="selectedPlayers(this.id)" class="player-cards" id="${name}", title="${name}, ${id}">${img}<div class="player-info"><p>Name: ${name}</p><p>Salary: ${playerID.Salary}</p></div></div>`;
 
+    }
   });
-  
-  team1.classList.remove("show") //used to remove from the dom
-  team2.classList.remove("show") // need to find a cleaner way...
-  document.getElementById("player-dropdown").innerHTML = playerList
+
+  team1.classList.remove("show"); //used to remove from the dom
+  team2.classList.remove("show"); // need to find a cleaner way...
+  document.getElementById("player-dropdown").innerHTML = playerList;
   document.getElementById("player-dropdown").classList.toggle("show-players");
   
 }
 
-function reset() {
-  document.getElementById(`teamOne-dropdown`).innerHTML =''
-  document.getElementById(`teamTwo-dropdown`).innerHTML =''
-  document.getElementById("player-dropdown").innerHTML =''
-  document.getElementById('teamOne').src = 'png/bball.svg'
-  document.getElementById('teamTwo').src = 'png/bball.svg'
+function resetPicks (teamReset) {
+  teamState[teamReset].picks = [];
+
 }
 
-function selectedPlayers(name){
-  let whichTeam = teamState.state
-  teamState[whichTeam].picks.includes(name) ? teamState[whichTeam].picks = teamState[whichTeam].picks.filter(player => player !== name)
-   : teamState[whichTeam].picks.push(name);
-  console.log(teamState.teamOne.picks, teamState[whichTeam].picks.includes(name))
+function resetDom() {
+  document.getElementById(`teamOne-dropdown`).innerHTML ='';
+  document.getElementById(`teamTwo-dropdown`).innerHTML ='';
+  document.getElementById("player-dropdown").innerHTML ='';
+  document.getElementById('teamOne').src = 'png/bball.svg';
+  document.getElementById('teamTwo').src = 'png/bball.svg';
+}
+//reset 
+function reset() { 
+  resetDom();
+  resetPicks('teamOne');
+  resetPicks('teamTwo');
 
-  
+}
+
+//add players if they have been selected but remove if they were
+function selectedPlayers (name) {
+  const whichTeam = teamState.state;
+  const isPlayerOnTeam = teamState[whichTeam].picks.includes(name);
+  if (isPlayerOnTeam) {teamState[whichTeam].picks = teamState[whichTeam].picks.filter(player => player !== name)} 
+  else {teamState[whichTeam].picks.push(name)} 
+
 }
 //ideas for menu: desktop keep team logos on side as you select players
 
